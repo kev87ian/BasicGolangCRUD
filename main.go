17c76b7687eb575a509be630ed9c95c39b6bdf8d
kev87ian <kelvinian87@gmail.com>
2024-02-28 11:39:15 +0300

    testing the gitignore

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "Hello friend!",
+		})
+	})
+
+	r.Run()
+}
